93ec1b7175ee5f10c074a05aaca3d4890282a21f
Veronika Bridnia <Veronika.Bridnia@thomsonreuters.com>
2019-11-25 17:11:27 +0200

    Inits project

---
+++ DiplomaSurvive/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiplomaSurvive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
