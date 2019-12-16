6c169d294d3ccefbd7d0fe69b378e9ca42c1be81
Milaila <milailatrens@gmail.com>
2019-12-17 00:56:39 +0200

    Refactores SimilarExamPage class

--- DiplomaSurvive/Exams/SimilarPageExam.cs
+++ DiplomaSurvive/Exams/SimilarPageExam.cs
@@ -8,18 +8,28 @@ namespace DiplomaSurvive
 {
     public class SimilarPageExam: ExamPage
     {
-        public SimilarPageExam(INumberGenerator generator, ExamPage nextPage, Button button, double leftCoef = 0, double rightCoef = 0)
+        public SimilarPageExam(INumberGenerator generator, ExamPage nextPage, string leftTitle = "", 
+            string rightTitle = "", double leftCoef = 0, double rightCoef = 0)
         {
-            Buttons.Add(new ExamButton(button, generator)
+            Buttons.Add(new ExamButton(generator)
             {
+                Title = leftTitle,
                 NextPage = nextPage,
                 DeductionCoefficient = leftCoef
             });
-            Buttons.Add(new ExamButton(button, generator)
+            Buttons.Add(new ExamButton(generator)
             {
+                Title = rightTitle,
                 NextPage = nextPage,
                 DeductionCoefficient = rightCoef
             });
+        }
+        public SimilarPageExam(ExamPage nextPage, ExamButton leftButton, ExamButton rightButton)
+        {
+            LeftButton = leftButton;
+            RightButton = rightButton;
+            leftButton.NextPage = nextPage;
+            rightButton.NextPage = nextPage;
         }
     }
 }
